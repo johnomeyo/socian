9637bbc84265a1c5d44648a7cdb0f27920d1be9e
John Omeyo <johnomeyo@gmail.com>
2024-11-11 17:58:10 +0300

    added the 30s countdown

--- seekers/lib/pages/homepage/timed_homepage.dart
+++ seekers/lib/pages/homepage/timed_homepage.dart
@@ -13,13 +13,21 @@ class TimedHomePage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(children: [
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(
-                        "https://media.wired.com/photos/59269cd37034dc5f91bec0f1/master/pass/GoogleMapTA.jpg"),
-                    fit: BoxFit.cover)),
-            height: size.height * 0.65,
+          Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://media.wired.com/photos/59269cd37034dc5f91bec0f1/master/pass/GoogleMapTA.jpg"),
+                        fit: BoxFit.cover)),
+                height: size.height * 0.65,
+              ),
+              const Positioned(
+                right: 30,
+                top: 30,
+                child: CountdownTimer()),
+            ],
           ),
           const SizedBox(height: 20),
           JobDetailsCard(
@@ -27,6 +35,71 @@ class TimedHomePage extends StatelessWidget {
           )
         ]),
       ),
+    );
+  }
+}
+
+class CountdownTimer extends StatefulWidget {
+  const CountdownTimer({super.key});
+
+  @override
+  CountdownTimerState createState() => CountdownTimerState();
+}
+
+class CountdownTimerState extends State<CountdownTimer>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  String get timerString {
+    Duration duration = _controller.duration! * _controller.value;
+    return '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 30),
+    );
+
+    _controller.reverse(from: 1.0);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 80,
+              height: 80,
+              child: CircularProgressIndicator(
+                value: _controller.value,
+                strokeWidth: 8.0,
+                valueColor: const AlwaysStoppedAnimation<Color>(Colors.purple),
+                backgroundColor: Colors.purple.shade100,
+              ),
+            ),
+            Text(
+              timerString,
+              style: const TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
