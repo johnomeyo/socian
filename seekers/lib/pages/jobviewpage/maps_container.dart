050c5ebc6ffd4aeeea794ba2879b9b460f438d9c
John Omeyo <johnomeyo@gmail.com>
2024-11-27 23:45:10 +0300

    changes on the maps container to add an image

--- seekers/lib/pages/jobviewpage/maps_container.dart
+++ seekers/lib/pages/jobviewpage/maps_container.dart
@@ -6,11 +6,16 @@ class MapsContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return SizedBox(
+    return Container(
       height: size.height * 0.35,
-      child: const Center(
-        child: Text('Map Image Placeholder'),
+      decoration:  BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        image: const DecorationImage(
+            image:  NetworkImage(
+                "https://www.shutterstock.com/shutterstock/photos/1661529616/display_1500/stock-vector-flat-street-map-with-direction-gps-navigation-on-road-town-with-pins-simple-information-scheme-of-1661529616.jpg"),
+            fit: BoxFit.cover),
       ),
+      
     );
   }
 }
